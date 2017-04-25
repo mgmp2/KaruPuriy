0893d97837303c2546c59c7b6c5812857cf2c7bb
ely211 <gonzalesm405@gmail.com>
2017-04-24 19:25:59 -0500

    validacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+function ClientData(nombre, apellido, correo, contrasena, selecDestino){
+  this.nombre = nombre;
+  this.contrasena = contrasena;
+  this.correo = correo;
+  this.contrasena = contrasena;
+  this.selecDestino = selecDestino;
+}
+
 var name1 = document.getElementById("name");
 var lastname = document.getElementById("lastname");
 var email = document.getElementById("input-email");
@@ -6,12 +14,10 @@ var optionsList = document.getElementById("select-destiny");
 
 var letterRegex = /^[a-zA-Z\s]*$/;
 var expRegEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-var message ="";
 
   function validateEachInput(expRegular, spanText1){
     var input = event.target;
-    var parentInput = input.parentNode;
-    console.log(parentInput);
+    var parentInput = input.parentNode;w
     if(!expRegular.test(input.value)){
       createTooltip(parentInput, spanText1);
     }else if(parentInput.lastElementChild.getAttribute("class") == "tooltiptext"){
@@ -48,31 +54,30 @@ window.addEventListener("load", function(){
    });
 
    optionsList.addEventListener("blur", function(){
-     console.log(event.target.parentNode);
-     var parentElement = event.target.parentNode
+     var parentElement = event.target.parentNode;
      if(optionsList.selectedIndex == 0){
        createTooltip(parentElement, "Selecciona una opción");
      }else {
-
+       parentElement.lastElementChild.style.display = "none";
      }
    })
 
-  document.getElementById("registrar").addEventListener("click", function (e){
-    e.preventDefault();
-    var mensaje;
-    var formSignUp = document.getElementById("signup-box");
-    var formChildren = formSignUp.children;
-    for(var i=1; i<formChildren.length; i++){
-      if(formChildren[i].getAttribute("class") == "input-box"){
-          if(formChildren[i].lastElementChild.getAttribute("class") == "tooltiptext inactive"){
-            console.log(formChildren[i].lastElementChild.getAttribute("class"));
-            mensaje = "Todo correcto";
-          } else if(!name.value || !lastname.value || !email.value ||!pass.value || options.value =="0"){
-              mensaje ="Se debe llenar todos los datos";
-        } else {
-            mensaje = "Todo correcto";
+   document.getElementById("registrar").addEventListener("click", function(){
+      event.preventDefault();
+      var newClient = new ClientData(name.value, lastname.value, email.value, pass.value, optionsList.options[optionsList.selectedIndex].text);
+      var parentElement = event.target.parentNode;
+      if(name1.value.trim().length == 0 || lastName.value.trim().length == 0 || email.value.trim().length == 0 || pass.value.trim().length == 0){
+        createTooltip(parentElement, "Todos los campos son obligatorios.");
+      }else {
+        parentElement.lastElementChild.style.display = "none";
+        if(pass.value == "123456" || pass.value == "password" || pass.value == "098754"){
+          createTooltip(parentElement, "Elige otra contraseña");
+        }else {
+          parentElement.lastElementChild.style.display = "none";
         }
+        localStorage.setItem("contacto", JSON.stringify(newClient));
+        // document.getElementById("client-bicis-form").reset();
       }
-    }
-  });
+
+    })
 })
