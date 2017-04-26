a8ed8f9e9351d7f52c7cfc8361085dabaf16251d
mgmp2 <miriammp1997@gmail.com>
2017-04-26 03:36:15 -0500

    css

--- assets/js/form.js
+++ assets/js/form.js
@@ -6,11 +6,6 @@ function ClientData(nombre, apellido, correo, contrasena, comentario){
     this.comentario = comentario;
 }
 
-function ToSignIn(correoRegistro, contrasenaRegistro){
-    this.correoRegistro = correoRegistro;
-    this.contrasenaRegistro = contrasenaRegistro;
-}
-
 var name1 = document.getElementById("name");
 var lastname = document.getElementById("lastname");
 var email = document.getElementById("input-email");
@@ -18,9 +13,6 @@ var pass = document.getElementById("input-password");
 var optionsList = document.getElementById("select-destiny");
 var coment = document.getElementById("coment");
 var allInput = document.querySelectorAll(".form-control");
-var emailSignIn = document.getElementById("email-sign-in");
-var passSignIn = document.getElementById("password-sign-in");
-
 
 var letterRegex = /^[a-zA-Z\s]*$/;
 var expRegEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
@@ -74,15 +66,6 @@ window.addEventListener("load", function(){
        validateEachInput(/\S{6}/, "Mínimo 6 carácteres")
    });
 
-    emailSignIn.addEventListener("input", function(){
-       validateEachInput(expRegEmail, "Ingresa el formato correcto")
-   });
-
-   passSignIn.addEventListener("input", function(){
-       validateEachInput(/\S{6}/, "Mínimo 6 carácteres")
-   });
-
-
     for (var i = 0; i < allInput.length; i++) {
       allInput[i].addEventListener("blur", capitalLetter);
     }
@@ -110,21 +93,5 @@ window.addEventListener("load", function(){
             parentElement.lastElementChild.style.display = "none";}
       }
     });
-
-    document.getElementById("log-in").addEventListener("click", function(e, showUser){
-        e.preventDefault();
-        var regToSignIn = new ToSignIn(emailSignIn.value, passSignIn.value);
-        var parentElement = event.target.parentNode;
-        if(emailSignIn.value.trim().length == 0 || passSignIn.value.trim() == 0){
-            createTooltip(parentElement, "todos los campos son obligatorios");
-        }else{
-          signIn.unshift(regToSignIn);
-          console.log(signIn);
-          localStorage.setItem("toSignIn", JSON.stringify(signIn));
-          if(parentElement.lastElementChild.getAttribute("class") == "tooltiptext"){
-                parentElement.lastElementChild.style.display = "none";}
-          userLS();
-        }
-    })
 
 });
