050e37fd7516afb16b4b0df5a00ca1848f7ebc91
Miriam Mendoza Pilco <miriammp1997@gmail.com>
2017-04-26 02:45:49 -0500

    add description for secInf

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,113 +23,143 @@
     lastScrollTop = currentScroll;
   }, false);
 
-  var arrayDep =   [{nombre: "Cajamarca", description: "Cajamarca, está situada a 2750 msnm en la margen este de la cadena oriental de la Cordillera de los Andes, en el valle interandino que forman los ríos Mashcon y Chonta",
+  var arrayDep =   [{nombre: "Cajamarca", description: "Está situada a 2750 msnm en la margen este de la cadena oriental de la Cordillera de los Andes, en el valle interandino que forman los ríos Mashcon y Chonta",
                       primerPaso: [ {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                     {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                     {salida:"6:00 p.m", llegada: "8:50", duracion: "15h", precio: "S/.100/120"},
                                     {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                     {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"}
                                   ],
-                      segundoPaso: [  {url: "assets/img/Cajamarca/queHacer/img-01.jpg", nombre: "Cajamar"},
-                                      {url: "assets/img/Cajamarca/queHacer/img-02.jpg", nombre: "Cajamar"},
-                                      {url: "assets/img/Cajamarca/queHacer/img-03.jpg", nombre: "Cajamar"}
+                      segundoPaso: [  {url: "assets/img/Cajamarca/queHacer/img-01.jpg", nombre: "Febrero-Carmaval de Cajamarca"},
+                                      {url: "assets/img/Cajamarca/queHacer/img-02.jpg", nombre: "Abril-Fiesta de las Cruces de Porcon"},
+                                      {url: "assets/img/Cajamarca/queHacer/img-03.jpg", nombre: "Junio-San Juan Bautista"}
                                    ],
                       tercerPaso: [ {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                    {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
                                     {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                   ],
                       cuartoPaso: [ {url: "assets/img/Cajamarca/plato/img-01.jpg", nombre:"adjkj"},
-                                    {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                                    {url: "assets/img/Cajamarca/plato/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                    {url: "assets/img/Cajamarca/plato/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                   ],
-                      quintoPaso: [ {url: "assets/img/Cajamarca/plato/img-01.jpg", nombre:"adjkj"},
-                                    {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                      quintoPaso: [ {url: "assets/img/Cajamarca/llevar/img-01.jpg", nombre:"adjkj"},
+                                    {url: "assets/img/Cajamarca/llevar/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                    {url: "assets/img/Cajamarca/llevar/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                   ]
 
                       },
-                     {nombre: "Tumbes", description: "cajamarca jajajajaja :v ",
+                     {nombre: "Tumbes", description: "Es una ciudad del extremo noroeste del Perú, situada cerca de la desembocadura del río Tumbes en el golfo de Guayaquil (océano Pacífico) ",
                      primerPaso: [ {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                    {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                    {salida:"6:00 p.m", llegada: "8:50", duracion: "15h", precio: "S/.100/120"},
                                    {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                    {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"}
                                  ],
-                     segundoPaso: [  {url: "assets/img/Cajamarca/queHacer/img-01.jpg", nombre: "Cajamar"},
-                                     {url: "assets/img/Cajamarca/queHacer/img-02.jpg", nombre: "Cajamar"},
-                                     {url: "assets/img/Cajamarca/queHacer/img-03.jpg", nombre: "Cajamar"}
+                     segundoPaso: [  {url: "assets/img/Tumbes/queHacer/img-01.jpg", nombre: "Octubre-Fiesta del Señor Cautivo de Ayabaca"},
+                                     {url: "assets/img/Tumbes/queHacer/img-02.jpg", nombre: "Visitar distrito de Zorritos"},
+                                     {url: "assets/img/Tumbes/queHacer/img-03.jpg", nombre: "Puedes ir a la playa o pescar"}
                                   ],
-                     tercerPaso: [ {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
-                                   {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                     tercerPaso: [ {url: "assets/img/Tumbes/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Tumbes/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Tumbes/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                  ],
-                     cuartoPaso: [ {url: "assets/img/Cajamarca/plato/img-01.jpg", nombre:"adjkj"},
-                                   {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                     cuartoPaso: [ {url: "assets/img/Tumbes/plato/img-01.jpg", nombre:"adjkj"},
+                                   {url: "assets/img/Tumbes/plato/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Tumbes/plato/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                  ],
-                     quintoPaso: [ {url: "assets/img/Cajamarca/plato/img-01.jpg", nombre:"adjkj"},
-                                   {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                     quintoPaso: [ {url: "assets/img/Tumbes/llevar/img-01.jpg", nombre:"adjkj"},
+                                   {url: "assets/img/Tumbes/llevar/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Tumbes/llevar/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                  ]
 
                      },
-                     {nombre: "Piura", description: "cajamarca jajajajaja :v ",
+                     {nombre: "Piura", description: "Está ubicada en el centro oeste de la región, en el valle del río Piura, al norte del desierto de Sechura, a 981 km al norte de Lima y próxima a la frontera con el Ecuador.  ",
                      primerPaso: [ {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                    {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                    {salida:"6:00 p.m", llegada: "8:50", duracion: "15h", precio: "S/.100/120"},
                                    {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                    {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"}
                                  ],
-                     segundoPaso: [  {url: "assets/img/Cajamarca/queHacer/img-01.jpg", nombre: "Cajamar"},
-                                     {url: "assets/img/Cajamarca/queHacer/img-02.jpg", nombre: "Cajamar"},
-                                     {url: "assets/img/Cajamarca/queHacer/img-03.jpg", nombre: "Cajamar"}
+                     segundoPaso: [  {url: "assets/img/Piura/queHacer/img-01.jpg", nombre: "Playa, surf, mochiler y vida nocturna"},
+                                     {url: "assets/img/Piura/queHacer/img-02.jpg", nombre: "Visitar al Parque Nacional de Amotape"},
+                                     {url: "assets/img/Piura/queHacer/img-03.jpg", nombre: "Playa, artesania y culturas"}
                                   ],
-                     tercerPaso: [ {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
-                                   {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                     tercerPaso: [ {url: "assets/img/Piura/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Piura/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Piura/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                  ],
-                     cuartoPaso: [ {url: "assets/img/Cajamarca/plato/img-01.jpg", nombre:"adjkj"},
-                                   {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                     cuartoPaso: [ {url: "assets/img/Piura/plato/img-01.jpg", nombre:"adjkj"},
+                                   {url: "assets/img/Piura/plato/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Piura/plato/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                  ],
-                     quintoPaso: [ {url: "assets/img/Cajamarca/plato/img-01.jpg", nombre:"adjkj"},
-                                   {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                     quintoPaso: [ {url: "assets/img/Piura/llevar/img-01.jpg", nombre:"adjkj"},
+                                   {url: "assets/img/Piura/llevar/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Piura/llevar/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                  ]
 
                      },
-                     {nombre: "Chiclayo", description: "cajamarca jajajajaja :v ",
+                     {nombre: "Chiclayo", description: "Está situada a 13 kilómetros de la costa del Pacífico y 770 kilómetros de la capital del país.",
                      primerPaso: [ {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                    {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                    {salida:"6:00 p.m", llegada: "8:50", duracion: "15h", precio: "S/.100/120"},
                                    {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                    {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"}
                                  ],
-                     segundoPaso: [  {url: "assets/img/Cajamarca/queHacer/img-01.jpg", nombre: "Cajamar"},
-                                     {url: "assets/img/Cajamarca/queHacer/img-02.jpg", nombre: "Cajamar"},
-                                     {url: "assets/img/Cajamarca/queHacer/img-03.jpg", nombre: "Cajamar"}
+                     segundoPaso: [  {url: "assets/img/Chiclayo/queHacer/img-01.jpg", nombre: "Visitar Plaza de Armas"},
+                                     {url: "assets/img/Chiclayo/queHacer/img-02.jpg", nombre: "Visitar Museo Tumbas Reales de Sipán"},
+                                     {url: "assets/img/Chiclayo/queHacer/img-03.jpg", nombre: "Paseo de Yourtuque"}
                                   ],
-                     tercerPaso: [ {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
-                                   {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                     tercerPaso: [ {url: "assets/img/Chiclayo/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Chiclayo/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Chiclayo/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                  ],
-                     cuartoPaso: [ {url: "assets/img/Cajamarca/plato/img-01.jpg", nombre:"adjkj"},
-                                   {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                     cuartoPaso: [ {url: "assets/img/Chiclayo/plato/img-01.jpg", nombre:"adjkj"},
+                                   {url: "assets/img/Chiclayo/plato/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Chiclayo/plato/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                  ],
-                     quintoPaso: [ {url: "assets/img/Cajamarca/plato/img-01.jpg", nombre:"adjkj"},
-                                   {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                     quintoPaso: [ {url: "assets/img/Chiclayo/llevar/img-01.jpg", nombre:"adjkj"},
+                                   {url: "assets/img/Chiclayo/llevar/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Chiclayo/llevar/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                  ]
 
                      },
-                     {nombre: "Trujillo", description: "cajamarca jajajajaja :v ",
+                     {nombre: "Trujillo", description: "Es la capital del departamento peruano de La Libertad, la ciudad más poblada del norte del Perú y la tercera ciudad más poblada del país; tiene al año 2015, una población estimada de 823.110 habitantes ",
                      primerPaso: [ {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                    {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                    {salida:"6:00 p.m", llegada: "8:50", duracion: "15h", precio: "S/.100/120"},
                                    {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"},
                                    {salida:"5:50 p.m", llegada: "8:50", duracion: "15h", precio: "S/.80/100"}
                                  ],
-                     segundoPaso: [  {url: "assets/img/Cajamarca/queHacer/img-01.jpg", nombre: "Cajamar"},
-                                     {url: "assets/img/Cajamarca/queHacer/img-02.jpg", nombre: "Cajamar"},
-                                     {url: "assets/img/Cajamarca/queHacer/img-03.jpg", nombre: "Cajamar"}
+                     segundoPaso: [  {url: "assets/img/Trujillo/queHacer/img-01.jpg", nombre: "Febrero-Carmaval de Cajamarca"},
+                                     {url: "assets/img/Trujillo/queHacer/img-02.jpg", nombre: "Cajamar"},
+                                     {url: "assets/img/Trujillo/queHacer/img-03.jpg", nombre: "Cajamar"}
                                   ],
-                     tercerPaso: [ {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
-                                   {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                     tercerPaso: [ {url: "assets/img/Trujillo/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Trujillo/hotel/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Trujillo/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                  ],
-                     cuartoPaso: [ {url: "assets/img/Cajamarca/plato/img-01.jpg", nombre:"adjkj"},
-                                   {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                     cuartoPaso: [ {url: "assets/img/Trujillo/plato/img-01.jpg", nombre:"adjkj"},
+                                   {url: "assets/img/Trujillo/plato/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Trujillo/plato/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                  ],
-                     quintoPaso: [ {url: "assets/img/Cajamarca/plato/img-01.jpg", nombre:"adjkj"},
-                                   {url: "assets/img/Cajamarca/hotel/img-01.jpg", nombre: "PRIMERhoTEL"}
+                     quintoPaso: [ {url: "assets/img/Trujillo/llevar/img-01.jpg", nombre:"adjkj"},
+                                   {url: "assets/img/Trujillo/llevar/img-01.jpg", nombre: "PRIMERhoTEL"},
+                                   {url: "assets/img/Trujillo/llevar/img-01.jpg", nombre: "PRIMERhoTEL"}
+
                                  ]
 
                      }];
